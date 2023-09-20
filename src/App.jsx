88f9a63c75fe8a160c73b2fc15934a66900b7eae
gautamabhinav <55458871+gautamabhinav@users.noreply.github.com>
2023-09-20 13:26:47 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
               <input hidden={login} type="text" name="" placeholder='Mobile Number or Email' id="" />
               <input hidden={login} type="text" name="" placeholder='Full Name' id="" />
               <input type="email" name="" id="" placeholder='Phone number, username, or email'/>
-              <input type="text" name="" id="" placeholder='Password' />
+              <input type="password" name="" id="" placeholder='Password' />
               <button>{login?"Sign in":"Sign up"}</button>
           <div id='footer'>
             {login?"Don't have account?":"have an account?"} <span onClick={loginSwitch}>{login?"Sign up":"Log in"}</span>
